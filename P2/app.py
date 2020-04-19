15751de9cd650bc47a5a9e9421b85c480c122b4a
Alverlaine <alexandre.verlaine@student.uclouvain.be>
2020-04-20 01:55:21 +0200

    création de +ieurs pages

--- P2/app.py
+++ P2/app.py
@@ -11,6 +11,18 @@ def hello_world():
 def cours():
     return render_template('cours.html')
 
+@app.route('/cours/cours1.html')
+def cours1():
+    return render_template('cours/cours1.html')
+
+@app.route('/cours/cours2.html')
+def cours2():
+    return render_template('cours/cours2.html')
+
+@app.route('/cours/cours3.html')
+def cours3():
+    return render_template('cours/cours3.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
