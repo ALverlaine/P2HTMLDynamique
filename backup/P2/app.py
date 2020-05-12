54b97fd48094788a200ac5020f2ad8a8e85a66cb
Alverlaine <alexandre.verlaine@student.uclouvain.be>
2020-05-12 03:28:22 +0200

    ending

--- backup/P2/app.py
+++ backup/P2/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for
-from sqlfunc import course_tasks_graph
+from sqlfunc import course_tasks_graph, task_result_graph, subm_time_graph
 import html.parser as htmlparser
 app = Flask(__name__)
 
@@ -9,28 +9,82 @@ parser = htmlparser.HTMLParser()
 def hello_world():
     return render_template('index.html')
 
+
 @app.route('/cours.html')
 def cours():
     return render_template('cours.html')
 
-@app.route('/cours1.html')
-def cours1():
+
+#COURS 1 _______________________________________________________________
+
+
+@app.route('/cours1_G1.html')
+def cours1_G1():
     l = course_tasks_graph("LSINF1252")
-    labels = l[1]
-    cours = '{}'.format(l[0])
-    val = l[2]
-    print(cours)
-    print(labels)
-    print(val)
-    return render_template('cours/cours1.html', label=labels, cours=cours, val=val)
+    return render_template('cours/cours1/cours1_G1.html', label=l[2], cours=l[1], val=l[3], type=l[0], bordercolor=l[4], background=l[5])
 
-@app.route('/cours2.html')
-def cours2():
-    return render_template('cours/cours2.html')
 
-@app.route('/cours3.html')
-def cours3():
-    return render_template('cours/cours3.html')
+@app.route('/cours1_G2.html')
+def cours1_G2():
+    l = task_result_graph("LSINF1252", "simple_stack")
+    return render_template('cours/cours1/cours1_G1.html', label=l[3], cours=l[1], val=l[4], type=l[2], bordercolor=l[5], background=l[6])
+
+
+@app.route('/cours1_G3.html')
+def cours1_G3():
+    l = subm_time_graph("LSINF1252", "2020-01-15T00:00", "2020-3-22T00:00")
+    return render_template('cours/cours1/cours1_G1.html', label=l[2], cours=l[1], val=l[3], type=l[0], bordercolor=l[4],
+                           background=l[5])
+
+#______________________________________________________________________
+
+#COURS 2_______________________________________________________________
+
+
+@app.route('/cours2_G1.html')
+def cours2_G1():
+    l = course_tasks_graph("LEPL1402")
+    return render_template('cours/cours1/cours1_G1.html', label=l[2], cours=l[1], val=l[3], type=l[0], bordercolor=l[4],
+                           background=l[5])
+
+
+@app.route('/cours2_G2.html')
+def cours2_G2():
+    l = task_result_graph("LEPL1402", "HanoiTower")
+    return render_template('cours/cours1/cours1_G1.html', label=l[3], cours=l[1], val=l[4], type=l[2], bordercolor=l[5], background=l[6])
+
+
+@app.route('/cours2_G3.html')
+def cours2_G3():
+    l = subm_time_graph("LEPL1402", "2020-02-15T00:00", "2020-02-22T00:00")
+    return render_template('cours/cours1/cours1_G1.html', label=l[2], cours=l[1], val=l[3], type=l[0], bordercolor=l[4],
+                           background=l[5])
+
+
+#______________________________________________________________________
+
+#COURS 3 ______________________________________________________________
+@app.route('/cours3_G1.html')
+def cours3_G1():
+    l = course_tasks_graph("LSINF1101-PYTHON")
+    return render_template('cours/cours1/cours1_G1.html', label=l[2], cours=l[1], val=l[3], type=l[0], bordercolor=l[4],
+                           background=l[5])
+
+
+@app.route('/cours3_G2.html')
+def cours3_G2():
+    l = task_result_graph("LSINF1101-PYTHON", "Quetelet")
+    return render_template('cours/cours1/cours1_G1.html', label=l[3], cours=l[1], val=l[4], type=l[2], bordercolor=l[5], background=l[6])
+
+
+@app.route('/cours3_G3.html')
+def cours3_G3():
+    l = subm_time_graph("LSINF1101-PYTHON", "2020-02-15T00:00", "2020-02-22T00:00")
+    return render_template('cours/cours1/cours1_G1.html', label=l[2], cours=l[1], val=l[3], type=l[0], bordercolor=l[4],
+                           background=l[5])
+
+
+#______________________________________________________________________
 
 
 if __name__ == '__main__':
--- backup/P2/sqlfunc.py
+++ backup/P2/sqlfunc.py
@@ -1,19 +1,83 @@
 import sqlite3
+
 def course_tasks_graph(cours):
     """
-    :param cours: une des cours de la db ->"LSINF1101-PYTHON", "LEPL1402", "LSINF1252"
-    :return: str du graph
+    :param cours: une str des cours de la db ->"LSINF1101-PYTHON", "LEPL1402", "LSINF1252"
+    :return: une liste :type de graphique, cours, label, valeurs
     """
     connection = sqlite3.connect("inginious.sqlite")
     cursor = connection.cursor()
-    label=[]
+    label = []
+    val = []
+    mix = []
+    back = 'rgb(255, 99, 132)'
+    border = 'rgb(255, 99, 132)'
     for row in cursor.execute("SELECT DISTINCT task from submissions WHERE course='{0}'".format(cours)):
-        label.append(row[0])
-
-    val=[]
-    for i in label:
-        for row in cursor.execute("SELECT count(task) from submissions WHERE task='{0}'".format(i)):
-            val.append(row[0])
+        mix.append([0, row[0]])
+    for i in mix:
+        for row in cursor.execute("SELECT count(task) from submissions WHERE task='{0}'".format(i[1])):
+            i[0] = row[0]
     connection.close()
+    mix.sort()
+    for i in mix:
+        val.append(i[0])
+        label.append(i[1])
+    return ['bar', cours, label, val, border, back]
 
-    return [cours, label, val]
+def task_result_graph(cours, task):
+    """
+    :param cours: Le nom du cours qui contient la tache (str)
+    :param task: Le nom de la tache dont on veut voir les statistiques (str)
+    :return: une liste :cours, tache,type de graph, les labels correspondants aux val, les val triees, couleur de bord et couleur de fond
+    """
+    connection = sqlite3.connect("inginious.sqlite")
+    cursor = connection.cursor()
+    val = []
+    label = []
+    border = []
+    back = []
+    mix = [[0,'failed',"rgb(255, 99, 132)","rgba(255, 99, 132, 0.2)"],
+           [0,'killed',"rgb(255, 205, 86)","rgba(255, 205, 86, 0.2)"],
+           [0,'success',"rgb(75, 192, 192)","rgba(75, 192, 192, 0.2)"],
+           [0,'overflow',"rgb(54, 162, 235)","rgba(54, 162, 235, 0.2)"],
+           [0,'timeout',"rgb(201, 203, 207)","rgba(201, 203, 207, 0.2)"],
+           [0,'crash',"rgb(153, 102, 255)","rgba(153, 102, 255, 0.2)"],
+           [0,'error',"rgb(255, 159, 64)","rgba(255, 159, 64, 0.2)"]]
+    for i, v in enumerate(mix):
+        for row in cursor.execute("SELECT count(task) from submissions WHERE course='{0}' and task='{1}' and "
+                                  "result='{2}'".format(cours, task, v[1])):
+            mix[i][0] = row[0]
+    connection.close()
+    mix.sort()
+    for i in mix:
+        label.append(i[1])
+        border.append(i[2])
+        back.append(i[3])
+        val.append(i[0])
+    return [cours, task,'bar', label, val, border, back]
+
+
+def subm_time_graph(cours,t1,t2):
+    """
+    :param cours:
+    :param t1: temps initial de la période a traiter incluse
+    :param t2: temps final de la periode a traiter excluse
+    :return:
+    """
+    connection = sqlite3.connect("inginious.sqlite")
+    cursor = connection.cursor()
+    val = []
+    label = []
+    back = 'rgb(255, 99, 132)'
+    border = 'rgb(255, 99, 132)'
+    for row in cursor.execute("SELECT substr(submitted_on,0,17) from submissions WHERE course='{0}'".format(cours)):
+        if t1 <= row[0] < t2:
+            if row[0] in label:
+                val[len(val) - 1] += 1
+            else:
+                label.append(row[0])
+                val.append(1)
+    connection.close()
+    return ['line', cours, label, val, border, back]
+
+
